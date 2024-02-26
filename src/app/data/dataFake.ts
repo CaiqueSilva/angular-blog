dc899de02c7a013f4612603b5580370bd9623ef3
caique <caique_silva94@hotmail.com>
2024-02-26 09:19:42 -0300

    finalização curso DIO

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photo":"aaa",
+    "title":"titulo da noticia",
+    "description":"descrição da noticia"
+  },
+  {
+    "id":2,
+    "photo":"aaa",
+    "title":"titulo da noticia",
+    "description":"descrição da noticia"
+  },
+  {
+    "id":3,
+    "photo":"aaa",
+    "title":"titulo da noticia",
+    "description":"descrição da noticia"
+  }
+]
